8f4f735606c8f6c296fbbf6be23091b3a4afbb6d
kchapelier <kevin.chapelier@gmail.com>
2015-05-02 09:58:17 +0200

    Implement down, press and up states, use Command construct, add some jsdoc blocks

---
+++ src/command.js
@@ -0,0 +1,42 @@
+/**
+ * Command constructor
+ * @param {string} id Command id
+ * @param {Object} options Command description (triggers and group)
+ * @constructor
+ */
+var Command = function (id, options) {
+    this.id = id;
+    this.group = options.group;
+    this.triggers = options.triggers || options.keys;
+
+    this.reset();
+};
+
+Command.prototype.value = null;
+
+Command.prototype.id = null;
+Command.prototype.group = null;
+Command.prototype.triggers = null;
+
+Command.prototype.activationTime = null;
+Command.prototype.active = null;
+
+Command.prototype.up = null;
+Command.prototype.down = null;
+Command.prototype.press = null;
+
+/**
+ * Reset the state of the command
+ * @private
+ */
+Command.prototype.reset = function () {
+    this.value = 0;
+    this.activationTime = 0;
+    this.active = false;
+
+    this.up = false;
+    this.down = false;
+    this.press = false;
+};
+
+module.exports = Command;
--- src/input.js
+++ src/input.js
@@ -1,9 +1,17 @@
 "use strict";
 
 var Keyboard = require('./handlers/keyboard'),
-    Gamepads = require('./handlers/gamepads');
+    Gamepads = require('./handlers/gamepads'),
+    Command = require('./command');
 
+/**
+ * Input constructor
+ * @param {Array} commands Array of command descriptions
+ * @constructor
+ */
 var Input = function (commands) {
+    this.activeCommands = [];
+
     this.setCommands(commands);
 
     this.currentTime = 0;
@@ -12,7 +20,7 @@ var Input = function (commands) {
     this.gamepads = Gamepads.isSupported ? Gamepads : null;
 };
 
-Input.prototype.currentTime = 0;
+Input.prototype.currentTime = null;
 Input.prototype.keyboard = null;
 Input.prototype.gamepads = null;
 
@@ -20,77 +28,73 @@ Input.prototype.commands = null;
 Input.prototype.inversedCommands = null;
 Input.prototype.activeCommands = null;
 
-Input.prototype.clearCurrentState = function () {
-    this.currentInput = {};
-    this.activeCommands = [];
-};
-
+/**
+ * Define the commands
+ * @param {Array} commands Array of command descriptions
+ */
 Input.prototype.setCommands = function (commands) {
-    this.clearCurrentState();
-    this.commands = commands || {};
-    this.createInverseLookupTable();
-};
-
-Input.prototype.createInverseLookupTable = function () {
-    var index,
-        keys,
+    var command = null,
+        commandId = null,
         i;
 
+    this.activeCommands.length = 0;
+
+    //TODO avoid the creation of all new objects below
+    //TODO should update the commands when possible, not recreate them
+
+    this.commands = {};
     this.inversedCommands = {};
 
-    for (index in this.commands) {
-        if (this.commands.hasOwnProperty(index)) {
-            keys = this.commands[index];
+    for (commandId in commands) {
+        if (commands.hasOwnProperty(commandId)) {
+            command = new Command(commandId, commands[commandId]);
 
-            for (i = 0; i < keys.keys.length; i++) {
-                this.inversedCommands[keys.keys[i]] = {
-                    command: index,
-                    group: keys.group,
-                    requireRevalidation: false,
-                    validationTime: 0
-                };
+            this.commands[commandId] = command;
+
+            for (i = 0; i < command.triggers.length; i++) {
+                this.inversedCommands[command.triggers[i]] = command;
             }
         }
     }
 };
 
-Input.prototype.processHandlers = function () {
-    this.processHandler(this.keyboard);
-    this.processHandler(this.gamepads);
-};
-
-Input.prototype.processHandler = function (handler) {
-    var input,
-        command;
-
-    handler.update();
-
-    for (input in handler.inputs) {
-        if (handler.inputs[input]) {
-            command = this.inversedCommands[input];
-
-            if (command) {
-                command.validationTime = this.currentTime;
-                command.value = handler.inputs[input];
-
-                if (command && this.activeCommands.indexOf(command) === -1) {
-                    this.activeCommands.push(command);
-                }
-            }
-        }
-    }
-};
-
-Input.prototype.update = function (dt) {
+/**
+ * Clear the current state of the commands
+ */
+Input.prototype.clearCurrentState = function () {
     var index;
+
+    this.activeCommands.length = 0;
+
+    for (index in this.commands) {
+        this.commands[index].reset();
+    }
+};
+
+/**
+ * Update the state of all the commands
+ * @param {number} dt Delta time, time elapsed since the last update in millisecond
+ */
+Input.prototype.update = function (dt) {
+    var command,
+        index;
 
     this.currentTime += dt;
 
     this.processHandlers();
 
-    // clear the currentInput array
     for (index in this.commands) {
-        this.currentInput[index] = 0;
+        command = this.commands[index];
+        command.active = false;
+        command.up = false;
+
+        if (command.value) {
+            command.down = command.press === false;
+            command.press = true;
+        } else {
+            command.down = false;
+            command.press = false;
+        }
     }
 
     this.clearInactiveCommands();
@@ -98,7 +102,45 @@ Input.prototype.update = function (dt) {
 };
 
 /**
+ * Process all the input handlers (keyboard and gamepads)
+ * @private
+ */
+Input.prototype.processHandlers = function () {
+    this.processHandler(this.keyboard);
+    this.processHandler(this.gamepads);
+};
+
+/**
+ * Process a specific input handler
+ * @private
+ * @param {Object} handler Input handler
+ */
+Input.prototype.processHandler = function (handler) {
+    var input,
+        command;
+
+    handler.update();
+
+    for (input in handler.inputs) {
+        command = this.inversedCommands[input];
+
+        if (command) {
+            command.value = handler.inputs[input];
+
+            if (command.value) {
+                command.validationTime = this.currentTime;
+
+                if (this.activeCommands.indexOf(command) === -1) {
+                    this.activeCommands.push(command);
+                }
+            }
+        }
+    }
+};
+
+/**
  * Clear all commands which were not updated this time
+ * @private
  */
 Input.prototype.clearInactiveCommands = function () {
     var activeCommands = this.activeCommands,
@@ -109,13 +151,15 @@ Input.prototype.clearInactiveCommands = function () {
         command = activeCommands[i];
 
         if (command.validationTime !== this.currentTime) {
+            command.up = true;
             activeCommands.splice(activeCommands.lastIndexOf(command), 1);
         }
     }
 };
 
 /**
- * Repopulate the currentInput array while taking into account the groups
+ * Activating the commands while taking into account the groups
+ * @private
  */
 Input.prototype.processActiveCommands = function () {
     var setGroup = {},
@@ -127,17 +171,25 @@ Input.prototype.processActiveCommands = function () {
 
         if (!command.group || !setGroup[command.group]) {
             setGroup[command.group] = true;
-            this.currentInput[command.command] = command.value;
+            command.active = true;
         }
     }
 };
 
+/**
+ * Attach the input handlers to a DOM element, add events
+ * @param {HTMLElement} element
+ */
 Input.prototype.attach = function (element) {
     element = element || document.body;
 
     this.keyboard.attach(element);
 };
 
+/**
+ * Detach the input handlers from a DOM element, remove events
+ * @param {HTMLElement} element
+ */
 Input.prototype.detach = function (element) {
     element = element || document.body;
 
